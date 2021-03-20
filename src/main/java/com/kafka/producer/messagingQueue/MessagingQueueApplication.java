f8f5971f74a73b42c3c17c83310a1a5fbf0dd65d
sethu <ssethucse@gmail.com>
2021-03-20 12:02:43 +0530

    Initial commit

---
+++ src/main/java/com/kafka/producer/messagingQueue/MessagingQueueApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.producer.messagingQueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingQueueApplication.class, args);
+	}
+
+}
